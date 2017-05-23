a5d7d717e846f9804a97c2737a04edc766b1732c
xiaoxiao <736343194@qq.com>
2017-05-23 09:46:04 +0800

    Vector的优化

---
+++ Vector/Vector2.cpp
@@ -0,0 +1,18 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+
+void Test()
+{
+	vector<int> v1;
+	v1.push_back(1);
+	v1.push_back(2);
+	v1.push_back(3);
+}
+
+int main()
+{
+	Test();
+	system("pause");
+	return 0;
+}
